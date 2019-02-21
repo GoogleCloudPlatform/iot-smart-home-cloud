26a8c2f0215252c0ba328202f04a3057c841d053
Dave Smith <smithdave@google.com>
2019-02-21 12:10:30 -0700

    Add license headers.
    Signed-off-by: Dave Smith <smithdave@google.com>
    Change-Id: I92faabc35c36fd54c91fa38c84d52aaaf9cce065

--- web/src/polyfills.ts
+++ web/src/polyfills.ts
@@ -1,17 +1,17 @@
 /**
- * This file includes polyfills needed by Angular and is loaded before the app.
- * You can add your own extra polyfills to this file.
+ * Copyright 2019, Google LLC
  *
- * This file is divided into 2 sections:
- *   1. Browser polyfills. These are applied before loading ZoneJS and are sorted by browsers.
- *   2. Application imports. Files imported after ZoneJS that should be loaded before your main
- *      file.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * The current setup is for so-called "evergreen" browsers; the last versions of browsers that
- * automatically update themselves. This includes Safari >= 10, Chrome >= 55 (including Opera),
- * Edge >= 13 on the desktop, and iOS 10 and Chrome on mobile.
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
- * Learn more in https://angular.io/guide/browser-support
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 /***************************************************************************************************
