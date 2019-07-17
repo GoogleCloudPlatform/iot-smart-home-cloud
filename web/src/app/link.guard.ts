92dcab4ae5d4094bfd3caefa67a623ec17a3883a
Dave Smith <smithdave@google.com>
2019-07-17 11:31:23 -0600

    Enable testing through OAuth Playground
    Remove hard-coded dependency on the OAuth redirect
    URI coming from Google Account Linking.
    
    Signed-off-by: Dave Smith <smithdave@google.com>
    Change-Id: I060b6bb618ffa59a5024e8332e48e2fe6a43850a

--- web/src/app/link.guard.ts
+++ web/src/app/link.guard.ts
@@ -27,13 +27,22 @@ export class LinkGuard implements CanActivate {
     state: RouterStateSnapshot): boolean {
     const params = next.queryParamMap;
 
+    const allowedHosts = [
+      'https://oauth-redirect.googleusercontent.com',
+      'https://developers.google.com'
+    ];
+
     // Validate account linking request parameters
     if (!params.has('client_id')
         || !params.has('response_type')
         || !params.has('redirect_uri')
+        // Client id matches expected value
         || params.get('client_id') !== environment.clientId
+        // Client is requesting an authorization code
         || params.get('response_type') !== 'code'
-        || !params.get('redirect_uri').startsWith('https://oauth-redirect.googleusercontent.com')) {
+        // Client requests a redirect to a known host
+        || !allowedHosts.some((element, _index, _array) => params.get('redirect_uri').startsWith(element))) {
+          console.error('Invalid Link Request', state.url)
           this.router.navigate(['error']);
           return false;
         }
